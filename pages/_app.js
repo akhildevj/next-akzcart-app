d2db093ad4fe8ee8d095e5cd90e9e48a8dd7dd13
akhildevj <akhildev.adj@gmail.com>
2022-04-11 21:07:25 +0530

    Fix: Font issue

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import NavBar from '../src/components/UI/NavBar';
 import { AuthProvider } from '../src/context/authContext';
 import { ReactNotifications } from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
+import '../styles/globals.css';
 import '../styles/styles.scss';
 
 const MyApp = ({ Component, pageProps }) => {
